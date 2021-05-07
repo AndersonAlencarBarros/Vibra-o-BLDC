887cbb944116f38c9700b1a85ca5a5527433838a
Henrique <henriquealvz@gmail.com>
2021-05-07 11:51:22 -0300

    adicionado html e arduino funcionando

--- server/index.js
+++ server/index.js
@@ -1,24 +1,27 @@
 const express = require('express');
-const socketIo= require('socket.io');
-const http=require('http');
-
-
 const app =express();
-const server = http.createServer(app);
-const io = socketIo.listen(server)
+
+const httpServer = require("http").createServer(app);
+
+const io = require("socket.io")(httpServer, {
+  cors: {
+    origin: "http://localhost:8080",
+  },
+});
 
 const SerialPort = require('serialport');
-const ReadLine = SerialPort.parses.Readline;
-const parser = newReadline();
+const Readline = SerialPort.parsers.Readline;
+const parser = new Readline();
+
 
 
 app.get('/', (req,res,next) =>{
 
-    res.sendFile(__dirname + '')//Segundo parametro e o caminho para o html 
+    res.sendFile(__dirname + '/index.html')//Segundo parametro e o caminho para o html 
 });// Criando a rota do servidor , Mandando o html a ser aberto pelo servidor
 
 
-const mySerial = new SerialPort('',{
+const mySerial = new SerialPort('COM3',{
    boudRate: 9600 
 }); //primeiro parametro e a porta conectada ao arduino segundo parametro e um objeto para configuração
 
@@ -35,10 +38,6 @@ mySerial.on('data',function(data){
 
 }); //Monstrar os dados recebidos pelo arduino
 
-server.listen(3000,()=>{
-    console.log('server na porta 3000')
-});//Iniciando o server, vai escutar na porta indicada
-
-
- 
-
+httpServer.listen(3000,()=>{
+     console.log('server na porta 3000')
+ });//Iniciando o server, vai escutar na porta indicada
